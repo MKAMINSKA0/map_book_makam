c6a8e8874eeb69774c04b736e98cdee70ee5856f
MKAMINSKA0 <167193924+MKAMINSKA0@users.noreply.github.com>
2024-04-23 14:25:55 +0200

    feat: create was added

--- utils/crud.py
+++ utils/crud.py
@@ -1,3 +1,10 @@
 def read(users: list[dict[str, str]])->None:
     for user in users[1:]:
         print(f'Twój znajomy {user["name"]} opublikował  {user["post"]} posty')
+
+    def add_user(Lista: list) -> None:
+        user_name = input("Podaj imię: ")
+        user_surname = input("Podaj nazwisko: ")
+        user_post = input("Podaj ilosc postów: ")
+        lista.append({'name': user_name, 'surname': user_surname, 'post': user_post})
+
