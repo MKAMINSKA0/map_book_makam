d78950362847a0a2d4af1f7f25d49751b6aac7a7
MKAMINSKA0 <167193924+MKAMINSKA0@users.noreply.github.com>
2024-05-16 14:36:03 +0200

    feat: map was added

--- utils/crud.py
+++ utils/crud.py
@@ -1,3 +1,7 @@
+import requests
+import folium
+from bs4 import BeautifulSoup
+
 def read(users: list[dict[str, str]]) -> None:
     for user in users[1:]:
         print(f'twój znajomy {user["name"]} opublikował {user["post"]} posty ')
@@ -9,11 +13,11 @@ def add_user(lista: list) -> None:
     user_post = input('podaj ile postów: ')
     lista.append({'name': user_name, 'surname': user_surname, 'post': user_post})
 
-    def search(users: list[dict[str, str]]) -> None:
-        user_name = input('Kogo szukasz? ')
-        for user in users[1:]:
-            if user['name'] == user_name:
-                print(f'Znaleziono użytkownika {user}  ')
+def search(users: list[dict[str, str]]) -> None:
+    user_name = input('Kogo szukasz? ')
+    for user in users[1:]:
+        if user['name'] == user_name:
+            print(f'Znaleziono użytkownika {user}  ')
 
 
 def remove_user(users: list[dict[str, str]]) -> None:
@@ -22,3 +26,40 @@ def remove_user(users: list[dict[str, str]]) -> None:
         if user['name'] == user_name:
             print(f'Znaleziono użytkownika {user['name']}  ')
             users.remove(user)
+
+def update_user(users: list[dict[str, str]]) -> None:
+    user_name = input('Kogo zaktualizować?: ')
+
+    for user in users[1:]:
+        if user['name'] == user_name:
+            user["name"] = input('Podaj nowe imie: ')
+            user["surname"] = input('podaj nowe nazwisko: ')
+            user['post'] = input('Podaj nową liczbę postów:')
+
+
+def map_single_users(imie, postow, miasto: str):
+    url = (f'https://pl.wikipedia.org/wiki/{miasto}')
+    response = requests.get(url)
+    response_html = BeautifulSoup(response.text, 'html.parser')
+    longitude = float(response_html.select('.longitude')[1].text.replace(',', '.'))
+    latitude = float(response_html.select('.latitude')[1].text.replace(',', '.'))
+    print(longitude, latitude)
+    map = folium.Map(location=[latitude, longitude], zoom_start=11)
+    folium.Marker(location=[latitude, longitude], popup=f'{imie},postów: {postow},\n{miasto}',
+                  incon=folium.Icon(color='blue')).add_to(map)
+    map.save(f'map_{miasto}.html')
+
+    def map_all_users(users):
+        map = folium.Map(location=[52,20], zoom_start=6)
+        for user in users:
+            url = (f'https://pl.wikipedia.org/wiki/{user['location']}')
+            response = requests.get(url)
+            response_html = BeautifulSoup(response.text, 'html.parser')
+            longitude = float(response_html.select('.longitude')[1].text.replace(',', '.'))
+            latitude = float(response_html.select('.latitude')[1].text.replace(',', '.'))
+            print(longitude, latitude)folium.Marker(location=[latitude, longitude], popup=f'{user['name']},postów: {user['location']},\n{user['location']}',
+                      icon=folium.Icon(color='blue')).add_to(map)
+
+
+
+        map.save(f'map.html')
